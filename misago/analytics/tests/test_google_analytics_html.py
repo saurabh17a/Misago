3ea371ff6c63352219238411d92415372b89afab
Saurabh Arya <saurabh.arya@mountblue.tech>
2020-07-29 04:08:24 +0530

    Added google annalytics and update google analytics test.

--- misago/analytics/tests/test_google_analytics_html.py
+++ misago/analytics/tests/test_google_analytics_html.py
@@ -3,9 +3,9 @@ from ...test import assert_contains, assert_not_contains
 
 
 @override_dynamic_settings(google_tracking_id=None)
-def test_tracking_script_is_not_included_if_tracking_id_is_not_set(db, client):
+def test_tracking_script_is_included_if_tracking_id_is_set(db, client):
     response = client.get("/")
-    assert_not_contains(response, "googletagmanager.com/gtag/js")
+    assert_contains(response, "googletagmanager.com/gtag/js")
 
 
 @override_dynamic_settings(google_tracking_id="UA-TEST")
